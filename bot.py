d6e564d146bb8002b08f60c01d097a5ef35f0eb8
Kacper4a <114807661+Kacper4a@users.noreply.github.com>
2023-02-26 12:56:06 +0100

    Add files via upload
    NO SPACY NEED IT

--- bot.py
+++ bot.py
@@ -1,22 +1,36 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-chatbot = ChatBot("Chatpot")
+my_bot = ChatBot (name='PyBot', read_only=True,
+                  logic_adapters=
+['chatterbot.logic.MathematicalEvaluation',
+                                'chatterbot.logic.BestMatch'])
 
-trainer = ListTrainer(chatbot)
-trainer.train([
-    "Hi",
-    "Welcome, friend 🤗",
-])
-trainer.train([
-    "Are you a plant?",
-    "No, I'm the pot below the plant!",
-])
+small_talk = ['hi there!',
+              'hi!',
+              'how do you do?',
+              'how are you?',
+              'i\'m cool.',
+              'fine, you?',
+              'always cool.',
+              'i\'m fine',
+              'glad to hear that.',
+              'i\'m ok',
+              'glad to hear that.',
+              'i feel awesome',
+              'excellent, glad to hear that.',
+              'not so good',
+              'sorry to hear that.',
+              'what\'s your name?',
+              'i\'m pybot. ask me a math question, please.']
 
-exit_conditions = (":q", "quit", "exit")
-while True:
-    query = input("> ")
-    if query in exit_conditions:
-        break
-    else:
-        print(f"🪴 {chatbot.get_response(query)}")
+math_talk_1 = ['pythagorean theorem',
+               'a squared plus b squared equals c squared.']
+
+math_talk_2 = ['law of cosines',
+               'c**2 = a**2 + b**2 - 2 * a * b * cos(gamma)']
+
+list_trainer = ListTrainer(my_bot)
+
+for item in (small_talk, math_talk_1, math_talk_2):
+    list_trainer.train(item)
